b1c415bc17bd1988a44cf0aa204ad2ff84122e6e
Guilherme Querentino <squerentino@gmail.com>
2022-05-22 20:49:28 -0300

    outro commit

---
+++ src/test/java/br/com/pedeai/msautenticacao/MsAutenticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedeai.msautenticacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAutenticacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
